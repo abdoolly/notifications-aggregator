967f36f9ddaa0533e3511b2b92b0ba7f84158c9c
Abdallah Gamal <abdoolly@gmail.com>
2021-06-14 01:41:14 +0200

    added helmet added response interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { ResponseInterceptor } from 'src/interceptors/response.interceptor';
 import { AppModule } from './app.module';
 import { AppService } from './app.service';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const appService = app.get<AppService>(AppService);
   await appService.seedDatabase();
+
+  app.useGlobalInterceptors(new ResponseInterceptor());
+  app.use(helmet());
 
   await app.listen(3000);
 }
